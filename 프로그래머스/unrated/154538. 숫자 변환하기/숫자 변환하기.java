d89faec5e4417fc705d0c35b94d94d63b061323c
juwoong kim <62009283+JuwoongKim@users.noreply.github.com>
2023-04-27 22:51:59 +0900

    [unrated] Title: 숫자 변환하기, Time: 20.26 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.java
@@ -2,22 +2,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int x, int y, int n) {
-       int[] dp = new int[y + 1];
-        for (int i = x; i < y + 1; i++) {
-            if (i != x && dp[i] == 0) {
-                dp[i] = -1;
-                continue;
-            }
-            if (i * 2 <= y) {
-                dp[i * 2] = (dp[i * 2] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 2]);
-            }
-            if (i * 3 <= y) {
-                dp[i * 3] = (dp[i * 3] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 3]);
-            }
-            if (i + n <= y) {
-                dp[i + n] = (dp[i + n] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i + n]);
-            }
+
+        
+        int answer = 0;
+        if(y==x) return 0;
+
+        int [] dp = new int [y+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+
+        dp[x] =0;
+        for(int i=x+1; i<=y; i++){     
+            if(i-n>=0 && dp[i-n]!=Integer.MAX_VALUE) dp[i] = dp[i-n]+1;
+            if(i%2==0 && dp[i/2]!=Integer.MAX_VALUE) dp[i] = Math.min(dp[i],dp[i/2]+1);
+            if(i%3==0 && dp[i/3]!=Integer.MAX_VALUE) dp[i] = Math.min(dp[i],dp[i/3]+1);
         }
-        return dp[y];
+        
+        
+        
+         //System.out.println(Arrays.toString(dp));
+        // System.out.println();
+        
+
+        if(dp[y]==Integer.MAX_VALUE)dp[y] = -1;
+        answer= dp[y];
+        return answer;
+        
+        
+
     }
 }
