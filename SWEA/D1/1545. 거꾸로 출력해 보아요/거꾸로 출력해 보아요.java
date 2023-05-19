44d4df3d1f260ee925c95805a25431161ede70d2
juwoong kim <wwong7247@gmail.com>
2023-05-19 14:33:59 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 105 ms, Memory: 18,400 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for( int i =N ; i>=0; i--){
+            System.out.print(i +" ");
+        }
+
+	}
+}
