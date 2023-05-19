857e4e7706483ab22ad62580ed894bb022c1d0ac
juwoong kim <wwong7247@gmail.com>
2023-05-20 01:40:37 +0900

    [D2] Title: 지그재그 숫자, Time: 109 ms, Memory: 18,652 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+
+public class Solution {
+
+	public static void main (String [] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		int T = Integer.parseInt(br.readLine());
+		for(int t =1; t<=T; t++) {	
+			
+			int N = Integer.parseInt(br.readLine());
+			
+			int sum =0 ; 
+			for(int i=1; i<=N; i++) {
+				if(i%2==0) sum -= i;
+				else sum +=i;
+			}
+			System.out.printf("#%d %d%n",t,sum);
+		}	
+	}
+	
+}
