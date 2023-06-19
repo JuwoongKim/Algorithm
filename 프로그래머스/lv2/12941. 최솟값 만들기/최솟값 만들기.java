b47a646c0307889693791eec1fab901a083cc755
juwoong kim <wwong7247@gmail.com>
2023-06-19 11:14:59 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.34 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int len = B.length - 1;
+        for(int i =0; i<B.length; i++){
+            answer += A[i]*B[len-i];
+        }
+        
+        return answer;
+    }
+}
