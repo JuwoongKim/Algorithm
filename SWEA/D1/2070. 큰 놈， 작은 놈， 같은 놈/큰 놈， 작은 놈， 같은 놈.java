c10552eb16f7bf317aeae95c28aeb9b998e78c77
juwoong kim <wwong7247@gmail.com>
2023-12-31 12:49:44 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 202 ms, Memory: 29,016 KB -BaekjoonHub

--- SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
@@ -1,23 +1,29 @@
-
-import java.util.*;
-import java.util.stream.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
 
-	public static void main (String [] args) throws Exception {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-		int N = Integer.parseInt(br.readLine());
-        
-		for(int i =0; i<N; i++) {
-			int [] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			
-			if (arr[0]>arr[1]) System.out.println(String.format("#%d %s",i+1, ">" ));
-			else if(arr[0]<arr[1]) System.out.println(String.format("#%d %s",i+1, "<"));
-			else System.out.println(String.format("#%d %s",i+1, "=" ));
+	public static void main(String[] args) throws IOException {
 
-		}	
-	}	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= n; i++) {
+			List<Integer> inputs = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+
+			String symbol;
+			if (inputs.get(0) > inputs.get(1)) {
+				symbol = ">";
+			} else if (inputs.get(0) == inputs.get(1)) {
+				symbol = "=";
+			} else {
+				symbol = "<";
+			}
+			System.out.println(String.format("#%d %s", i, symbol));
+		}
+	}
 }
